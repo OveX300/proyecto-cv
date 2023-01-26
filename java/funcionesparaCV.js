772ec0e9f6778af510c6ff1b5ce31721fbd59e9e
Damian Romero <damian_rom123@outlook.com>
2023-01-26 07:58:42 -0300

    primer commit

---
+++ java/funcionesparaCV.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('Boton').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#0DA930';
+});
+
+document.getElementById('Boton1').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#008B8B';
+});
